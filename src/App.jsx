5c1e50a0e4177c27835c4dd10fb8a227462d3f21
anamaydev <anamaydev@gmail.com>
2025-05-20 17:47:01 +0530

    style: add custom CSS reset file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 /* todo:
 *   [x] create custom css variables
-*   [x] set up custom font */
+*   [x] set up custom font
+*   [x] add reset.css file */
 
 function App() {
   return (
