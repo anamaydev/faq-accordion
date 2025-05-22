0320d20e7c9d8334db26a5a38d5f2a1c103483fa
anamaydev <anamaydev@gmail.com>
2025-05-22 11:08:09 +0530

    style: add responsive styling for accordion on mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {nanoid} from 'nanoid'
 import './App.css'
 import mobileBackground from './assets/images/background-pattern-mobile.svg'
 import tabletBackground from './assets/images/background-pattern-desktop.svg'
+import plusIcon from './assets/images/icon-plus.svg'
+import minusIcon from './assets/images/icon-minus.svg'
+import startIcon from './assets/images/icon-star.svg'
 
 /* todo:
 *   [x] create custom css variables
@@ -28,16 +31,23 @@ function App() {
 
   function FaqListItem({faqData, setSelectedFaq}) {
     return faqData.map((faq, index)=>(
-      <li key={nanoid()}>
+      <li className="app__faq-list-item" key={nanoid()}>
         <input
+          className="app__faq-list-input"
           type="radio"
           id={`faq-${index}`}
           name="faq"
           value={faq.question}
           checked={selectedFaq === faq.question}
           onChange={(e) => setSelectedFaq(e.target.value)} />
-        <label htmlFor={`faq-${index}`}>{faq.question}</label>
-        {selectedFaq === faq.question && <p>{faq.answer}</p>}
+        <div className="app__faq-list-item-content">
+          <div className="app__faq-list-question-wrapper">
+            <label className="app__faq-list-question" htmlFor={`faq-${index}`}>{faq.question}</label>
+            {selectedFaq === faq.question && <img src={plusIcon} alt=""/>}
+            {selectedFaq !== faq.question && <img src={minusIcon} alt=""/>}
+          </div>
+          {selectedFaq === faq.question && <p className="app__faq-list-answer">{faq.answer}</p>}
+        </div>
       </li>
     ))
   }
@@ -46,7 +56,8 @@ function App() {
     <>
       <div className="app-background">
         <picture>
-          <source srcSet={tabletBackground} media="(min-width: 600px)"/>
+          {/*<source srcSet={tabletBackground} media="(min-width: 600px)"/>*/}
+          <source srcSet={tabletBackground} media="(min-width: 768px)"/>
           <img className="app-background__image" src={mobileBackground} alt=""/>
         </picture>
       </div>
@@ -54,7 +65,7 @@ function App() {
       <main className="app">
         <section className="app__faq">
           <div className="app__faq-header">
-            <img className="app__faq-header-icon" src="#" alt=""/>
+            <img className="app__faq-header-icon" src={startIcon} alt=""/>
             <h1 className="app__faq-header">FAQs</h1>
           </div>
           <ul className="app__faq-list">
